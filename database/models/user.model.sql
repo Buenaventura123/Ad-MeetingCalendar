85a9dc55222d197f00a988fee5f8330a3d4f92d6
Buenaventura123 <202310536@fit.edu.ph>
2025-07-04 19:48:45 +0800

    feat: added other user info

--- database/models/user.model.sql
+++ database/models/user.model.sql
@@ -1,8 +1,9 @@
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    password TEXT NOT NULL,
-    role VARCHAR(100)
+CREATE TABLE IF NOT EXISTS public."users" (
+    id                  uuid            NOT NULL PRIMARY KEY DEFAULT gen_random_uuid (),
+    first_name          varchar(225)    NOT NULL,
+    middle_name         varchar(225),
+    last_name           varchar(225)    NOT NULL,
+    password            varchar(225)    NOT NULL,
+    username            varchar(225)    NOT NULL UNIQUE,
+    role                varchar(225)    NOT NULL
 );
